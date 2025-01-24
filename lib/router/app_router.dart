5d708f7223628cfb980464ecc14f48a7c659913b
arg0nath <vamakris07+github@gmail.com>
2025-01-24 17:19:38 +0200

    add animated fav button & android predictive back

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -43,32 +43,21 @@ class Router {
     switch (settings.name) {
       case app_const.TYPE_DETAILS_SCREEN_PAGE_ROUTE_NAME:
         final TypeDetailsScreenArguments args = settings.arguments! as TypeDetailsScreenArguments;
-
         return MaterialPageRoute(builder: (context) => TypeDetailsScreen(typeDetails: args.typeDetails));
-
-      //     return PageTransition<dynamic>(
-      //         type: app_const.PAGE_TRANSITION_TYPE_FADE, settings: const RouteSettings(name: app_const.TYPE_DETAILS_SCREEN_PAGE_ROUTE_NAME), child: TypeDetailsScreen(typeDetails: args.typeDetails));
+      //return PageTransition<dynamic>(type: app_const.PAGE_TRANSITION_TYPE_FADE, settings: const RouteSettings(name: app_const.TYPE_DETAILS_SCREEN_PAGE_ROUTE_NAME), child: TypeDetailsScreen(typeDetails: args.typeDetails));
       case app_const.TYPE_SELECTION_SCREEN_PAGE_ROUTE_NAME:
         return PageTransition<dynamic>(
             type: app_const.PAGE_TRANSITION_TYPE_FADE, settings: const RouteSettings(name: app_const.TYPE_SELECTION_SCREEN_PAGE_ROUTE_NAME), child: const TypeSelectionScreen());
-
       case app_const.HOME_SCREEN_PAGE_ROUTE_NAME:
         return PageTransition<dynamic>(type: app_const.PAGE_TRANSITION_TYPE_FADE, settings: const RouteSettings(name: app_const.HOME_SCREEN_PAGE_ROUTE_NAME), child: const HomeScreen());
-
       case app_const.FAVORITES_ROUTE_NAME:
         return PageTransition<dynamic>(type: app_const.PAGE_TRANSITION_TYPE_FADE, settings: const RouteSettings(name: app_const.FAVORITES_ROUTE_NAME), child: const FavoritesScreen());
-
       case app_const.WELCOME_SCREEN_ROUTE_NAME:
         return PageTransition<dynamic>(type: app_const.PAGE_TRANSITION_TYPE_FADE, settings: const RouteSettings(name: app_const.WELCOME_SCREEN_ROUTE_NAME), child: const WelcomeScreen());
       case app_const.POKEMON_DETAILS_SCREEN_ROUTE_NAME:
         final PokemonDetailsScreenArguments args = settings.arguments! as PokemonDetailsScreenArguments;
-        return PageTransition<dynamic>(
-            type: app_const.PAGE_TRANSITION_TYPE_FADE,
-            settings: const RouteSettings(name: app_const.POKEMON_DETAILS_SCREEN_ROUTE_NAME),
-            child: PokemonDetailsScreen(
-              pokemon: args.pokemon,
-              selectedTypeName: args.selectedTypeName,
-            ));
+        return MaterialPageRoute(builder: (context) => PokemonDetailsScreen(pokemon: args.pokemon, selectedTypeName: args.selectedTypeName));
+      //return PageTransition<dynamic>(type: app_const.PAGE_TRANSITION_TYPE_FADE,settings: const RouteSettings(name: app_const.POKEMON_DETAILS_SCREEN_ROUTE_NAME),child: PokemonDetailsScreen(pokemon: args.pokemon,selectedTypeName: args.selectedTypeName,));
       default:
         return PageTransition<dynamic>(type: app_const.PAGE_TRANSITION_TYPE_FADE, settings: const RouteSettings(name: app_const.HOME_SCREEN_PAGE_ROUTE_NAME), child: const HomeScreen());
     }

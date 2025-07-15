cb1f9cf538149baf248f7e088e2986c274754c1f
arg0nath <vamakris07+github@gmail.com>
2025-07-16 01:11:28 +0300

    feat: Enhance error handling and navigation in Pokemon and Type details pages

--- lib/features/type_details/presentation/bloc/type_details_state.dart
+++ lib/features/type_details/presentation/bloc/type_details_state.dart
@@ -3,13 +3,9 @@ part of 'type_details_bloc.dart';
 @freezed
 class TypeDetailsState with _$TypeDetailsState {
   const factory TypeDetailsState.initial() = _Initial;
-
   const factory TypeDetailsState.loading() = _Loading;
-
   const factory TypeDetailsState.loaded(TypeDetails typeDetails) = _Loaded;
-
   const factory TypeDetailsState.error(String message) = _Error;
-
   const factory TypeDetailsState.readyToProceedToPokemonDetails(ProceedingStatus status) = _ReadyToProceedToPokemonDetails;
 }
 

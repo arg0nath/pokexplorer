fa68121f67b83c0daa285bfd38b159726c589f40
arg0nath <vamakris07+github@gmail.com>
2025-08-08 18:04:32 +0300

    feat: Update PokemonPreviewDto to use base image URL and enhance TypeDetailsDto logging

--- lib/features/type_details/data/dtos/pokemon_preview_dto.dart
+++ lib/features/type_details/data/dtos/pokemon_preview_dto.dart
@@ -2,6 +2,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:pokexplorer/config/typedefs/typedefs.dart';
 import 'package:pokexplorer/core/common/constants/app_const.dart';
 import 'package:pokexplorer/core/common/utils/pokemon/extract_poke_preview_id.dart';
+import 'package:pokexplorer/core/common/utils/pokemon/get_poke_image_by_id.dart';
 import 'package:pokexplorer/features/type_details/domain/entities/pokemon_preview.dart';
 
 part 'pokemon_preview_dto.freezed.dart';
@@ -24,7 +25,12 @@ abstract class PokemonPreviewDto extends PokemonPreview with _$PokemonPreviewDto
   factory PokemonPreviewDto.fromJson(DataMap json) => _$PokemonPreviewDtoFromJson(json);
 
   PokemonPreview toEntity() {
-    return PokemonPreview(id: extractPokemonPreviewId(url), name: name, url: url);
+    final int id = extractPokemonPreviewId(url);
+    return PokemonPreview(
+      id: id,
+      name: name,
+      url: getPokemonBaseImageById(id),
+    );
   }
 
   static PokemonPreviewDto fromEntity(PokemonPreview entity) {
--- lib/features/type_details/data/dtos/type_details_dto.dart
+++ lib/features/type_details/data/dtos/type_details_dto.dart
@@ -1,4 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:pokexplorer/config/logger/my_log.dart';
 import 'package:pokexplorer/config/typedefs/typedefs.dart';
 import 'package:pokexplorer/features/type_details/data/dtos/pokemon_preview_dto.dart';
 import 'package:pokexplorer/features/type_details/domain/entities/pokemon_preview.dart';
@@ -21,6 +22,7 @@ abstract class TypeDetailsDto with _$TypeDetailsDto {
 extension TypeDetailsDtoX on TypeDetailsDto {
   TypeDetails toEntity() {
     final List<PokemonPreview> pokemons = pokemon.map((DataMap entry) => PokemonPreviewDto.fromJson(entry['pokemon'] as DataMap).toEntity()).toList();
+    myLog('Converted TypeDetailsDto to TypeDetails: id=$id, name=$name, pokemons=${pokemons}');
     return TypeDetails(
       id: id,
       name: name,

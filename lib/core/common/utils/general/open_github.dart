748290c7579eb14de91a56bc0193039074d0aec5
arg0nath <32592902+arg0nath@users.noreply.github.com>
2025-08-13 17:14:47 +0300

    Chore: Update GitHub URL in openGitHub function to point to the correct disclaimer file

--- lib/core/common/utils/general/open_github.dart
+++ lib/core/common/utils/general/open_github.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void openGitHub(BuildContext context, {bool isDisclaimer = false}) async {
-  final Uri githubUri = Uri.parse(isDisclaimer ? 'https://github.com/arg0nath/pokexplorer/tree/dev?tab=readme-ov-file#-credits' : 'https://github.com/arg0nath');
+  final Uri githubUri = Uri.parse(isDisclaimer ? 'https://github.com/arg0nath/pokexplorer/blob/main/DISCLAIMER.md' : 'https://github.com/arg0nath');
 
   if (await canLaunchUrl(githubUri)) {
     await launchUrl(githubUri, mode: LaunchMode.externalApplication);

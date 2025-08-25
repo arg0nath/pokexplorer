215e19b0b5799aefa9db43e54a8ea5c90666863e
arg0nath <32592902+arg0nath@users.noreply.github.com>
2025-08-25 22:46:20 +0300

    refactor: Minor changes refactoring local data source for theme settings with SharedPreferences

--- lib/config/theme/theme.dart
+++ lib/config/theme/theme.dart
@@ -1,5 +1,5 @@
+export 'package:pokexplorer/config/theme/data/datasource/local/theme_local_datasource.dart';
 export 'package:pokexplorer/config/theme/data/repository/theme_repo_impl.dart';
-export 'package:pokexplorer/config/theme/data/source/local/theme_local_datasource.dart';
 export 'package:pokexplorer/config/theme/domain/repository/theme_repo.dart';
 export 'package:pokexplorer/config/theme/domain/usecase/get_theme_usecase.dart';
 export 'package:pokexplorer/config/theme/domain/usecase/set_theme_usecase.dart';

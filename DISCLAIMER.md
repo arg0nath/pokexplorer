f7bfa48540b5789933fefba1a555047dd024e8c7
arg0nath <32592902+arg0nath@users.noreply.github.com>
2025-08-25 23:58:22 +0300

    feat: Update version and enhance disclaimer with copyright information

--- DISCLAIMER.md
+++ DISCLAIMER.md
@@ -2,4 +2,6 @@
 - Pokémon names, types, stats, images, and GIFs are property of **The Pokémon Company**.  
 - Pokéball icons from: [Flaticon Pokémon Icons](https://www.flaticon.com/free-icons/pokemon) — Created by Nikita Golubev, Roundicons Freebies, Shahzama Ahmad, Saepul Nahwan.  
 - The **Pokéxplorer logo** and **Pokéxplorer phrase** was created by me using a modified version of the official Pokémon logo.
+- The **Pokéxplorer PlayStore & Github Forest Image** was created by me using ChatGPT.
+- Check more about content & copyright in the application settings.
 - [LICENSE](LICENSE)
---
+++ lib/features/settings/settings.dart
@@ -0,0 +1,8 @@
+export 'package:pokexplorer/features/settings/data/datasources/local/settings_local_datasource.dart';
+export 'package:pokexplorer/features/settings/data/repo/settings_repo_impl.dart';
+export 'package:pokexplorer/features/settings/domain/repo/settings_repo.dart';
+export 'package:pokexplorer/features/settings/domain/usecases/get_copyright_option.dart';
+export 'package:pokexplorer/features/settings/domain/usecases/get_terms_option.dart';
+export 'package:pokexplorer/features/settings/domain/usecases/set_copyright_option.dart';
+export 'package:pokexplorer/features/settings/domain/usecases/set_terms_option.dart';
+export 'package:pokexplorer/features/settings/presentation/bloc/settings_bloc.dart';

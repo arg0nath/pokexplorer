d9eb7ee8ee68f90da25a808c315c50a4c699cd6a
arg0nath <32592902+arg0nath@users.noreply.github.com>
2025-08-25 23:58:27 +0300

    feat: Add settings import to dependency injection for improved settings management

--- lib/core/services/di_imports.dart
+++ lib/core/services/di_imports.dart
@@ -4,6 +4,7 @@ import 'package:pokexplorer/config/theme/theme.dart';
 import 'package:pokexplorer/core/common/utils/general/init_db.dart';
 import 'package:pokexplorer/features/on_boarding/on_boarding.dart';
 import 'package:pokexplorer/features/pokemon_details/pokemon_details.dart';
+import 'package:pokexplorer/features/settings/settings.dart';
 import 'package:pokexplorer/features/type_details/type_details.dart';
 import 'package:pokexplorer/features/type_selection/type_selection.dart';
 import 'package:pokexplorer/features/user_favorites/user_favorites.dart';
